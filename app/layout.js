9d94f263e7657837e5b8638b1a8adbb400614d62
buqueslari <buqueslari@gmail.com>
2026-02-25 14:26:19 -0300

    update tag

--- app/layout.js
+++ app/layout.js
@@ -125,14 +125,14 @@ export default function RootLayout({ children }) {
         {/* Rafaela Flores Manager Script */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17974967890"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17976698485"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17974967890');
+            gtag('config', 'AW-17976698485');
           `}
         </Script>
       </head>
